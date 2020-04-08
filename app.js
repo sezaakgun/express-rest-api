e8fbaec19b92848195ecd14db92a3138a7617d18
sezaakgun <sezaak3@gmail.com>
2020-04-08 13:18:37 +0100

    version controll change from v1 to v1.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/uploads', express.static('uploads'));
-app.use('/v1/auth/', authRoute);
-app.use('/v1/users/', userRoute);
+app.use('/v1.0/auth/', authRoute);
+app.use('/v1.0/users/', userRoute);
 
 module.exports = app;
